cf0547dd658c549b4518fc83f2da774695f18b68
oceanly020 <oceanly020@gmail.com>
2018-09-15 15:56:44 +0800

    test i2

--- src/all_BDD.h
+++ src/all_BDD.h
@@ -3156,7 +3156,7 @@ row_matrix_CSR_multiply(struct CS_matrix_idx_v_arr *row, struct matrix_CSR *matr
 struct CS_matrix_idx_v_arr *
 row_matrix_CSR_multiply_bysort(struct CS_matrix_idx_v_arr *row, struct matrix_CSR *matrix_CSR) {
   struct CS_matrix_idx_v_arr *tmp = NULL;
-  struct CS_matrix_idx_v *vs[200000];
+  struct CS_matrix_idx_v *vs[100000];
   // uint32_t max_CSR = MAX_VAL_RATE*data_allr_nums*data_allr_nums;
   // struct matrix_Tri_express **Tri_arr = xmalloc(max_CSR*sizeof(struct matrix_Tri_express *));
   uint32_t vs_count = 0;
@@ -3176,6 +3176,7 @@ row_matrix_CSR_multiply_bysort(struct CS_matrix_idx_v_arr *row, struct matrix_CS
             vs[vs_count]->idx = row_matrix->idx_vs[j]->idx;
             vs[vs_count] = elem_tmp;
             vs_count ++;
+
           }     
           elem_tmp = NULL;
         }
